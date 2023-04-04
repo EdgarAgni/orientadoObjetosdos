f78d72194676a393f463b62baad382ae225624b9
Edgar_Agni <edgar_agni@hotmail.com>
2023-04-03 23:44:51 -0600

    generalizando en un solo JS y probando que funcione el codigo

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import {CuentaAhorro} from './CuentaAhorro.js';
-import {CuentaCorriente} from './CuentaCorriente.js';
+import { Cuenta } from './Cuenta.js';
+/*import {CuentaAhorro} from './CuentaAhorro.js';
+import {CuentaCorriente} from './CuentaCorriente.js';*/
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const cuentaDeLeonardo = new Cuenta(cliente, '1', '001', 0);
+const cuentaDeMaria = new Cuenta(cliente2,'2','002', 0);
 
-const cuentaAhorroLeonardo = new CuentaAhorro(cliente, "3", "001", 0);
+const cuentaAhorroLeonardo = new Cuenta(cliente, "3", "001", 0);
 
 console.log(cuentaDeLeonardo);
-console.log(cuentaAhorroLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(150);
+console.log(cuentaDeLeonardo.verSaldo());
+cuentaDeLeonardo.retirarDeCuenta(50);
+console.log(cuentaDeLeonardo.verSaldo());
 
+console.log(cuentaAhorroLeonardo);
+cuentaAhorroLeonardo.depositoEnCuenta(500);
+console.log(cuentaAhorroLeonardo.verSaldo());
+cuentaAhorroLeonardo.retirarDeCuenta(20);
+console.log(cuentaAhorroLeonardo.verSaldo());
 
 
 
